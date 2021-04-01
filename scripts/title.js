997b657ba9a0dbefd3dde383a3a258df9d78dcad
qRega <73142820+qRega@users.noreply.github.com>
2021-04-02 02:09:49 +0300

    Add files via upload

---
+++ scripts/title.js
@@ -0,0 +1,41 @@
+﻿if(document.addEventListener){
+	document.addEventListener("DOMContentLoaded", function(){
+		loaded();
+	});
+} else if(document.attachEvent){
+	document.attachEvent("onreadystatechange", function(){
+		loaded();
+	});
+}
+
+function loaded(){
+	
+	setInterval(loop, 300);
+
+}
+
+var x = 0;
+
+var titleText = [ "|",
+					"Ｒ|",
+					"Ｒ |",
+					"Ｒ Ｅ|",
+					"Ｒ Ｅ |",
+					"Ｒ Ｅ Ｇ|",
+					"Ｒ Ｅ Ｇ |",
+					"Ｒ Ｅ Ｇ Ａ|",
+					"Ｒ Ｅ Ｇ Ａ |",
+					"Ｒ Ｅ Ｇ Ａ .|",
+					"Ｒ Ｅ Ｇ Ａ . |",
+					"Ｒ Ｅ Ｇ Ａ . Ｆ|",
+					"Ｒ Ｅ Ｇ Ａ . Ｆ |",	
+					"Ｒ Ｅ Ｇ Ａ . Ｆ Ａ|",
+					"Ｒ Ｅ Ｇ Ａ . Ｆ Ａ |",	
+					"Ｒ Ｅ Ｇ Ａ . Ｆ Ａ Ｎ|",
+					"Ｒ Ｅ Ｇ Ａ . Ｆ Ａ Ｎ |",	];
+
+function loop(){
+
+	document.getElementsByTagName("title")[0].innerHTML = titleText[x++%titleText.length];
+
+}
